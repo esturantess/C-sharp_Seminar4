a21eb1a1ec461afd100e56c57d9f517705a0682e
Polina <ms.maylin@inbox.ru>
2023-01-24 21:39:04 +0300

    Task4 added

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+Random rand = new Random();
+int[] array = new int[8];
+
+for (int i = 0; i <= 7; i++)
+{
+    array[i] = rand.Next(0, 2);
+}
+
+foreach (var i in array)
+{
+    Console.Write(i + " ");
+}
