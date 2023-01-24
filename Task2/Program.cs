5c65d2e53875573c8e2637f4fd6302fe6a6b8880
Polina <ms.maylin@inbox.ru>
2023-01-24 20:58:18 +0300

    Task2 added

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+int InputNumber(string str)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+
+        System.Console.WriteLine("Введено не корректное число, попробуйте ещё раз!");
+    }
+    return number;
+}
+
+int num = InputNumber("Введите число: ");
+string numString = Convert.ToString(num);
+int size = numString.Length;
+System.Console.WriteLine($"Количество цифр в числе: {size}");
