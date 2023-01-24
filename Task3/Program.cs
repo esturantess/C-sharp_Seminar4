633b5088f9d1429d1e0138467f6d8b722d74e171
Polina <ms.maylin@inbox.ru>
2023-01-24 21:25:39 +0300

    Task3 added

---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int InputNumber(string str)
+{
+    int number;
+    string? text;
+    while (true)
+    {
+        Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Введено некорректное число.");
+    }
+    return number;
+}
+
+void Factorial(int count)
+{
+    int answer = 1;
+    for (int i = 1; i < count + 1; i++)
+    {
+        answer *= i;
+    }
+    System.Console.WriteLine(answer);
+}
+
+int N = InputNumber("Введите число N: ");
+Factorial(N);
